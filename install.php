e7d90597f23bb1816d3385efcd246ad8b73f5d18
tecobhavini <76998567+tecobhavini@users.noreply.github.com>
2021-01-07 10:56:43 +0530

    install.php
    Updated File

--- install.php
+++ install.php
@@ -1,1 +1,14 @@
+<?php
+// Set variables for our request
+$shop = $_GET['shop'];
 
+$api_key = "2fb25ec956ed6d241c5cc10fc3ce1ab2";
+$scopes = "read_orders,write_products,write_script_tags,read_orders";
+$redirect_uri = "https://bvn-test-app.herokuapp.com/generate_token.php";
+
+// Build install/approval URL to redirect to
+$install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
+
+// Redirect
+header("Location: " . $install_url);
+die();
